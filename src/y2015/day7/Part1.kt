87691116fe3b6577e061401b8189de341108bccd
pwh-pwh <plmoknpwh@163.com>
2024-02-07 21:15:29 +0800

    y2016 day7 part1 simplified

--- src/y2015/day7/Part1.kt
+++ src/y2015/day7/Part1.kt
@@ -2,82 +2,50 @@ package y2015.day7
 
 import java.io.File
 
-/**
- * @Auther: coderpwh
- * @Date: 2024/2/7 16:13
- * @Description:
- */
 fun main() {
-    val map = mutableMapOf<String,Int>()
+    val map = mutableMapOf<String, Int>()
     val data = File("src/y2015/day7/day7.txt").readLines().toMutableList()
-    while (!data.isEmpty()) {
-        data.iterator().let {
-                iterator ->
+
+    while (data.isNotEmpty()) {
+        data.iterator().let { iterator ->
             while (iterator.hasNext()) {
                 val line = iterator.next()
                 try {
-                    if (line.contains("AND")) {
-                        doAction(Action.AND,line,map)
-                    } else if (line.contains("OR")) {
-                        doAction(Action.OR,line,map)
-                    } else if (line.contains("LSHIFT")) {
-                        doAction(Action.LSHIFT,line,map)
-                    } else if (line.contains("RSHIFT ")) {
-                        doAction(Action.RSHIFT,line,map)
-                    } else if (line.contains("NOT")) {
-                        doAction(Action.NOT,line,map)
-                    } else {
-                        doAction(Action.SET,line,map)
+                    val action = when {
+                        line.contains("AND") -> Action.AND
+                        line.contains("OR") -> Action.OR
+                        line.contains("LSHIFT") -> Action.LSHIFT
+                        line.contains("RSHIFT") -> Action.RSHIFT
+                        line.contains("NOT") -> Action.NOT
+                        else -> Action.SET
                     }
+                    doAction(action, line, map)
                     iterator.remove()
-                } catch (e:Exception) {
-//                println("Error on line: $line")
+                } catch (e: Exception) {
+                    // Ignore errors
                 }
             }
-
-
         }
     }
 
-    map.forEach {
-        println(it)
-    }
+    map.forEach { println(it) }
 }
 
 enum class Action {
     AND, OR, LSHIFT, RSHIFT, NOT, SET
 }
 
-fun doAction(action: Action,cmd:String,map:MutableMap<String,Int>) {
-    val (input,output) = cmd.split(" -> ")
+fun doAction(action: Action, cmd: String, map: MutableMap<String, Int>) {
+    val (input, output) = cmd.split(" -> ")
     val value = when (action) {
-        Action.AND -> {
-            val (a,b) = input.split(" AND ")
-                a.getValue(map) and b.getValue(map)
-        }
-        Action.OR -> {
-            val (a,b) = input.split(" OR ")
-            a.getValue(map) or b.getValue(map)
-        }
-        Action.LSHIFT -> {
-            val (a,b) = input.split(" LSHIFT ")
-            a.getValue(map) shl b.toInt()
-        }
-        Action.RSHIFT -> {
-            val (a,b) = input.split(" RSHIFT ")
-            a.getValue(map) shr b.toInt()
-        }
-        Action.NOT -> {
-            val a = input.split("NOT ")[1]
-            a.getValue(map).inv()
-        }
-        Action.SET -> {
-            input.getValue(map)
-        }
+        Action.AND -> input.split(" AND ").run { this[0].getValue(map) and this[1].getValue(map) }
+        Action.OR -> input.split(" OR ").run { this[0].getValue(map) or this[1].getValue(map) }
+        Action.LSHIFT -> input.split(" LSHIFT ").run { this[0].getValue(map) shl this[1].toInt() }
+        Action.RSHIFT -> input.split(" RSHIFT ").run { this[0].getValue(map) shr this[1].toInt() }
+        Action.NOT -> input.split("NOT ")[1].getValue(map).inv()
+        Action.SET -> input.getValue(map)
     }
     map[output] = value
 }
 
-private fun String.getValue(map:Map<String,Int>):Int {
-    toIntOrNull()?.let { return it } ?: return map[this]!!
-}
+private fun String.getValue(map: Map<String, Int>): Int = this.toIntOrNull() ?: map[this]!!
