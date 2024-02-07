131e871d40a8f5f0e6d5e8e1d80f03cc4f0f5ae1
pwh-pwh <plmoknpwh@163.com>
2024-02-07 21:13:31 +0800

    y2016 day7 part1

--- src/y2015/day7/Part1.kt
+++ src/y2015/day7/Part1.kt
@@ -9,29 +9,75 @@ import java.io.File
  */
 fun main() {
     val map = mutableMapOf<String,Int>()
-    File("src/y2015/day7/day7.txt")
-        .forEachLine {
-            line ->
-            if (line.contains("AND")) {
-                doAction(Action.AND,line,map)
-            } else if (line.contains("OR")) {
-                doAction(Action.OR,line,map)
-            } else if (line.contains("LSHIFT")) {
-                doAction(Action.LSHIFT,line,map)
-            } else if (line.contains("RSHIFT ")) {
-                doAction(Action.RSHIFT,line,map)
-            } else if (line.contains("NOT")) {
-                doAction(Action.NOT,line,map)
-            } else {
-                doAction(Action.SET,line,map)
+    val data = File("src/y2015/day7/day7.txt").readLines().toMutableList()
+    while (!data.isEmpty()) {
+        data.iterator().let {
+                iterator ->
+            while (iterator.hasNext()) {
+                val line = iterator.next()
+                try {
+                    if (line.contains("AND")) {
+                        doAction(Action.AND,line,map)
+                    } else if (line.contains("OR")) {
+                        doAction(Action.OR,line,map)
+                    } else if (line.contains("LSHIFT")) {
+                        doAction(Action.LSHIFT,line,map)
+                    } else if (line.contains("RSHIFT ")) {
+                        doAction(Action.RSHIFT,line,map)
+                    } else if (line.contains("NOT")) {
+                        doAction(Action.NOT,line,map)
+                    } else {
+                        doAction(Action.SET,line,map)
+                    }
+                    iterator.remove()
+                } catch (e:Exception) {
+//                println("Error on line: $line")
+                }
             }
+
+
         }
+    }
+
+    map.forEach {
+        println(it)
+    }
 }
 
 enum class Action {
     AND, OR, LSHIFT, RSHIFT, NOT, SET
 }
 
-fun doAction(action: Action,cmd:String,map:Map<String,Int>) {
+fun doAction(action: Action,cmd:String,map:MutableMap<String,Int>) {
+    val (input,output) = cmd.split(" -> ")
+    val value = when (action) {
+        Action.AND -> {
+            val (a,b) = input.split(" AND ")
+                a.getValue(map) and b.getValue(map)
+        }
+        Action.OR -> {
+            val (a,b) = input.split(" OR ")
+            a.getValue(map) or b.getValue(map)
+        }
+        Action.LSHIFT -> {
+            val (a,b) = input.split(" LSHIFT ")
+            a.getValue(map) shl b.toInt()
+        }
+        Action.RSHIFT -> {
+            val (a,b) = input.split(" RSHIFT ")
+            a.getValue(map) shr b.toInt()
+        }
+        Action.NOT -> {
+            val a = input.split("NOT ")[1]
+            a.getValue(map).inv()
+        }
+        Action.SET -> {
+            input.getValue(map)
+        }
+    }
+    map[output] = value
+}
 
+private fun String.getValue(map:Map<String,Int>):Int {
+    toIntOrNull()?.let { return it } ?: return map[this]!!
 }
---
+++ src/y2015/day7/Partd.kt
@@ -0,0 +1,52 @@
+package y2015.day7
+
+import java.io.File
+
+var wires = mutableMapOf<String, Int>()
+
+fun valueOf(v: String): Int {
+    return v.toIntOrNull() ?: wires[v]!!
+}
+
+fun parseExpression(ex: String): Int {
+    val data = ex.trim().split(" ")
+    return when {
+        data.size == 1 -> valueOf(data[0])
+        data.size == 2 -> valueOf(data[1]) xor 65535
+        else -> {
+            val (a, e, b) = data
+            val aValue = valueOf(a)
+            val bValue = valueOf(b)
+            when (e) {
+                "AND" -> aValue and bValue
+                "OR" -> aValue or bValue
+                "LSHIFT" -> aValue shl bValue
+                "RSHIFT" -> aValue shr bValue
+                else -> throw IllegalArgumentException("Unknown operation: $e")
+            }
+        }
+    }
+}
+
+fun evaluate(line: String) {
+    val (ex, n) = line.split(" -> ")
+    wires[n] = parseExpression(ex)
+}
+
+fun main() {
+    val data = File("src/y2015/day7/day7.txt").readLines().toMutableList()
+    while (data.isNotEmpty()) {
+        data.iterator().let { iterator ->
+            while (iterator.hasNext()) {
+                val d = iterator.next()
+                try {
+                    evaluate(d)
+                    iterator.remove()
+                } catch (e: Exception) {
+                    continue
+                }
+            }
+        }
+    }
+    println(wires["a"])
+}
