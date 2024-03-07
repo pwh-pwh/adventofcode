a7e6f085a1e906f61cf20f9f059e41e3178f9262
pwh-pwh <plmoknpwh@163.com>
2024-03-07 17:35:05 +0800

    y2015 day15 part1 unimpl

--- src/y2015/day15/Part1.kt
+++ src/y2015/day15/Part1.kt
@@ -8,16 +8,59 @@ import java.io.File
  * @Description:
  */
 fun main() {
-    data class Record(val capacity: Int, val durability: Int, val flavor: Int, val texture: Int, val calories: Int)
+    //Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
+    //Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3
     var data = File("src/y2015/day15/day15.txt")
         .readLines()
         .map {
             str ->
-            val numberRegex = """\d+""".toRegex()
+            val numberRegex = """-?\d+""".toRegex()
             val (capacity, durability, flavor,texture,calories) = numberRegex.findAll(str).map {
                 it.value.toInt()
             }.toList()
             Record(capacity, durability, flavor, texture, calories)
         }
+    doCal(data, mutableListOf(),100)
+    println("max: $maxValue")
+}
+var maxValue = Int.MIN_VALUE
+data class Record(val capacity: Int, val durability: Int, val flavor: Int, val texture: Int, val calories: Int)
 
+private fun doCal(data: List<Record>,dis:MutableList<Int>,t:Int) {
+    if(data.size == dis.size+1) {
+        dis.add(t)
+        var calR = calR(data, dis)
+        if (calR > maxValue) {
+            maxValue = calR
+            if (calR==125000000) {
+                //打印 dis
+                println(dis)
+            }
+        }
+
+        dis.removeLast()
+        return
+    }
+    for (i in 0..t) {
+        dis.add(i)
+        doCal(data, dis, t - i)
+        dis.removeAt(dis.size - 1)
+    }
+}
+
+fun calR(data: List<Record>,dis:List<Int>):Int {
+    var capacity = 0
+    var durability = 0
+    var flavor = 0
+    var texture = 0
+    data.zip(dis)
+        .forEach {
+            (record, dis) ->
+            capacity += record.capacity * dis
+            durability += record.durability * dis
+            flavor += record.flavor * dis
+            texture += record.texture * dis
+        }
+    if (capacity==0||durability==0||flavor==0||texture==0) return 0
+    return capacity*durability*flavor*texture
 }
