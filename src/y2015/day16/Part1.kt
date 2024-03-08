92d544d042ff8167133a4fc2a030683e83a7d4c5
pwh-pwh <plmoknpwh@163.com>
2024-03-08 16:30:05 +0800

    y2015 day16 part1 impl

--- src/y2015/day16/Part1.kt
+++ src/y2015/day16/Part1.kt
@@ -8,6 +8,26 @@ import java.io.File
  * @Description:
  */
 fun main() {
-    File("src/y2015/day16/input.txt")
-        .readLines()
+    val validMap = mapOf("children" to 3, "cats" to 7, "samoyeds" to 2,
+        "pomeranians" to 3, "akitas" to 0, "vizslas" to 0,
+        "goldfish" to 5, "trees" to 3, "cars" to 2, "perfumes" to 1)
+    var rl = File("src/y2015/day16/input.txt")
+        .readLines().map {
+            input ->
+            val regex = """(\w+): (\d+)""".toRegex()
+            val matches = regex.findAll(input)
+            var vm = mutableMapOf<String,Int>()
+            for (match in matches) {
+                val (key, value) = match.destructured
+                vm[key] = value.toInt()
+            }
+            vm
+        }.withIndex()
+        .filter {
+            (_, vm) ->
+            vm.all { (key, value) ->
+                validMap[key] == value
+            }
+        }.toList()
+    println(rl)
 }
