569013a0112ed6f5a2436a49e9db0fc7c03c53c6
pwh-pwh <plmoknpwh@163.com>
2024-03-08 16:07:30 +0800

    y2015 day16 part1 unimpl

---
+++ src/y2015/day16/Part1.kt
@@ -0,0 +1,13 @@
+package y2015.day16
+
+import java.io.File
+
+/**
+ * @Auther: coderpwh
+ * @Date: 2024/3/8 15:59
+ * @Description:
+ */
+fun main() {
+    File("src/y2015/day16/input.txt")
+        .readLines()
+}
