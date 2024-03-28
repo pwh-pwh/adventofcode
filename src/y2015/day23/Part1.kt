bb50de175b05fc16da91c567ff455ac8052ed859
pwh-pwh <plmoknpwh@163.com>
2024-03-28 15:49:18 +0800

    y2015 day23 part1  unimpl

---
+++ src/y2015/day23/Part1.kt
@@ -0,0 +1,12 @@
+package y2015.day23
+
+import java.io.File
+
+/**
+ * @Auther: coderpwh
+ * @Date: 2024/3/28 15:45
+ * @Description:
+ */
+fun main() {
+    val s = File("src/y2015/day23/input.txt").readLines()
+}
