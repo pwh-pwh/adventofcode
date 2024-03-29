a28c4e306d199bd0711c334a4c8c705fb0970a4e
pwh-pwh <plmoknpwh@163.com>
2024-03-29 16:43:09 +0800

    y2015 day24 part1 and part2 impl

--- src/y2015/day18/Part1.kt
+++ src/y2015/day18/Part1.kt
@@ -1,3 +1,5 @@
+package y2015.day18
+
 import java.io.File
 
 
--- src/y2015/day24/Part1.kt
+++ src/y2015/day24/Part1.kt
@@ -1,17 +1,50 @@
 package y2015.day24
 
-import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
 
-/**
- * @Auther: coderpwh
- * @Date: 2024/3/28 17:29
- * @Description:
- */
 fun main() {
-    val numList = File("src/y2015/day24/input.txt")
-        .readLines()
-        .map {
-            it.toInt()
-        }
+    val input = "src/y2015/day24/input.txt"
+    val startTime = System.currentTimeMillis()
 
+    val lines = Files.readAllLines(Paths.get(input))
+    val list = lines.map { it.toInt() }
+    val sum = list.sum()
+
+    // for part 1 use 3, part 2 use 4
+    val each = sum / 4
+
+    val sortedList = list.sorted()
+    println("${sortedList.size}, $sum, $each")
+    println(sortedList)
+    var minV = Long.MAX_VALUE
+    for (i in sortedList.indices) {
+        val cp = ArrayList(sortedList)
+        cp.removeAt(i)
+        val filled = fill(cp, 0, each)
+        val lFilled = filled.map { it.toLong() }
+        val product = lFilled.fold(1L) { acc, num -> acc * num }
+        println("product $product")
+        if(product<minV) {
+            minV = product
+        }
+    }
+    println("minV: $minV")
+
+    val endTime = System.currentTimeMillis()
+    println("Time ${endTime - startTime}ms")
+}
+
+fun fill(list: List<Int>, bucket: Int, max: Int): List<Int> {
+    var currentBucket = bucket
+    val nums = mutableListOf<Int>()
+
+    for (i in list.indices.reversed()) {
+        if (currentBucket + list[i] <= max) {
+            currentBucket += list[i]
+            nums.add(list[i])
+        }
+    }
+
+    return nums
 }
